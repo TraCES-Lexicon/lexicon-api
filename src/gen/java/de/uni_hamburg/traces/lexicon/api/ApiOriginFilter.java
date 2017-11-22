913a4a72f3fdc117170b58f11d8fc2ce90c6a0d0
Stephan Druskat <mail@sdruskat.net>
2017-11-22 18:01:18 +0100

    Disallow any control methods apart from GET

--- src/gen/java/de/uni_hamburg/traces/lexicon/api/ApiOriginFilter.java
+++ src/gen/java/de/uni_hamburg/traces/lexicon/api/ApiOriginFilter.java
@@ -5,18 +5,25 @@ import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 
-@javax.annotation.Generated(value = "class io.wagger.api.codegen.languages.JavaJerseyServerCodegen", date = "2017-11-21T15:37:30.051+01:00")
+/**
+ * // TODO Add description
+ *
+ * @author Stephan Druskat <[mail@sdruskat.net](mailto:mail@sdruskat.net)>
+ * 
+ */
 public class ApiOriginFilter implements javax.servlet.Filter {
-    public void doFilter(ServletRequest request, ServletResponse response,
-            FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.addHeader("Access-Control-Allow-Origin", "*");
-        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-        res.addHeader("Access-Control-Allow-Headers", "Content-Type");
-        chain.doFilter(request, response);
-    }
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("Access-Control-Allow-Origin", "*");
+		res.addHeader("Access-Control-Allow-Methods", "GET");
+		res.addHeader("Access-Control-Allow-Headers", "Content-Type");
+		chain.doFilter(request, response);
+	}
 
-    public void destroy() {}
+	public void destroy() {
+	}
 
-    public void init(FilterConfig filterConfig) throws ServletException {}
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
 }
