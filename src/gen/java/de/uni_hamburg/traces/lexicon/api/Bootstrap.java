a0c07588c041429ffaf89361b354e7a429f456c8
Stephan Druskat <mail@sdruskat.net>
2017-11-27 14:51:40 +0100

    Clean up Bootstrap

--- src/gen/java/de/uni_hamburg/traces/lexicon/api/Bootstrap.java
+++ src/gen/java/de/uni_hamburg/traces/lexicon/api/Bootstrap.java
@@ -1,10 +1,9 @@
 package de.uni_hamburg.traces.lexicon.api;
 
-import io.swagger.jaxrs.config.SwaggerContextService;
+import io.swagger.jaxrs.config.SwaggerContextService; 
 import io.swagger.models.*;
 
 import javax.servlet.http.HttpServlet;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
@@ -22,12 +21,12 @@ public class Bootstrap extends HttpServlet {
 
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		Info info = new Info().title("TraCES Ge'ez Lexicon API").description(
-				"An API that allows users to return accumulated results from different resources from their queries. ## **NOTE** SNAPSHOT, Do not implement! ## **NOTE** Will be superseded by https://swaggerhub.com/apis/sdruskat/traces-lexicon-api/1.0.0-SNAPSHOT ")
+		Info info = new Info()
+				.title("TraCES Ge'ez Lexicon API")
+				.description("An API that allows users to return accumulated results from different resources from their queries. ## **NOTE** SNAPSHOT, Do not implement! ## **NOTE** Will be superseded by https://swaggerhub.com/apis/sdruskat/traces-lexicon-api/1.0.0-SNAPSHOT ")
 				.contact(new Contact().name("Stephan Druskat").url("http://sdruskat.net"))
 				.license(new License().name("License: tba"));
 
-		ServletContext context = config.getServletContext();
 		Swagger swagger = new Swagger().info(info);
 
 		new SwaggerContextService().withServletConfig(config).updateSwagger(swagger);
