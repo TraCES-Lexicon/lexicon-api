995ac31c4c309c3a9e6d227cb8a5b0f8d983a4c8
Stephan Druskat <mail@sdruskat.net>
2017-11-28 10:38:39 +0100

    Add basic docs

--- src/main/java/de/uni_hamburg/traces/lexicon/api/QueryApiService.java
+++ src/main/java/de/uni_hamburg/traces/lexicon/api/QueryApiService.java
@@ -1,16 +1,6 @@
 package de.uni_hamburg.traces.lexicon.api;
 
-import de.uni_hamburg.traces.lexicon.api.*;
-import de.uni_hamburg.traces.lexicon.model.*;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-
-import de.uni_hamburg.traces.lexicon.model.Result;
-
-import java.util.List;
 import de.uni_hamburg.traces.lexicon.api.NotFoundException;
-
-import java.io.InputStream;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
